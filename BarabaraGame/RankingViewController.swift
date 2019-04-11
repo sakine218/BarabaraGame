8c0d67954cf65d87ff598d49eaaf7fb0ead6f3cf
sakine218 <snishibayashi218@gmail.com>
2019-04-11 11:16:35 +0900

    first commit

---
+++ BarabaraGame/RankingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RankingViewController.swift
+//  BarabaraGame
+//
+//  Created by Sakine Nishibayashi on 2019/03/31.
+//  Copyright © 2019 Sakine Nishibayashi. All rights reserved.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+    
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+    
+    let defaults: UserDefaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTop() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
